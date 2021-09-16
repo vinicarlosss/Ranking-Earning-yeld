96c815a8d9bf36a0c85abc48e54ae5356ded16bd
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-09-16 17:45:02 -0300

    adicionado forma de inserção de dados automatizada

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 function calculaEarningYeld(ebit,valor_mercado,divida_liquida){
     let ey = ebit/(valor_mercado+divida_liquida)*100;
     return ey.toFixed(2)
@@ -26,25 +27,55 @@ async function selectEmpresa(empresa){
 }
 
 async function insertEmpresa(empresa){
-    const conn = await connect();
-    const sql = 'INSERT INTO empresa (ticker,nome_empresa,ebit,valor_mercado,divida_liquida) VALUES(?,?,?,?,?);'
-    const values = [empresa.ticker, empresa.nome_empresa, empresa.ebit, empresa.valor_mercado, empresa.divida_liquida];
-    await conn.query(sql,values);
-    const ey = calculaEarningYeld(empresa.ebit, empresa.valor_mercado, empresa.divida_liquida);
-    const sql2 = 'INSERT INTO ranking (ticker,earning_yeld) VALUES(?,?);'
-    const values2 = [empresa.ticker,ey];
-    await conn.query(sql2,values2);
+    for(let i = 0; i<empresa.length; i++){
+        let periodo = ["2021_T2", "2021_T1", "2020_T4", "2020_T3"];
+        let resultado = undefined
+        let ebit = 0
+        await axios.get(`https://www.okanebox.com.br/api/demonstrativofinanceiro/${empresa[i][0]}/`)
+        .then(function(data){
+            resultado = data.data
+            let c = 0
+
+            while (c < resultado.length){
+                if (resultado[c]['DS_CONTA'] == "Resultado Antes do Resultado Financeiro e dos Tributos"){
+                    for(let j = 0; j<4; j++){
+                        ebit += resultado[c][periodo[j]];
+                    }
+                    break
+                }else{
+                    c += 1
+                };
+            };
+            ebit = (ebit/1000000).toFixed(2);
+        });
+        const conn = await connect();
+        const sql = 'INSERT INTO empresa (ticker,nome_empresa,ebit,valor_mercado,divida_liquida) VALUES(?,?,?,?,?);'
+        resultado = undefined;
+        await axios.get(`https://www.okanebox.com.br/api/analisefundamentalista/${empresa[i][0]}`)
+        .then(function(data){
+            resultado = data.data
+        });
+        const values = [empresa[i][0], empresa[i][1], ebit, (resultado[2].value/1000000).toFixed(2), (resultado[9].value/1000000).toFixed(2)];
+        await conn.query(sql,values);
+        const ey = calculaEarningYeld(ebit, (resultado[2].value/1000000),(resultado[9].value/1000000));
+        const sql2 = 'INSERT INTO ranking (ticker,earning_yeld) VALUES(?,?);'
+        const values2 = [empresa[i][0],ey];
+        await conn.query(sql2,values2);
+    };
 };
 
-async function updateEmpresa(ticker, empresa){
-    const conn = await connect();
-    const sql = 'UPDATE empresa SET nome_empresa = ?, ebit=?,valor_mercado=?,divida_liquida=? WHERE ticker=?';
-    const values =  [empresa.nome_empresa,empresa.ebit,empresa.valor_mercado, empresa.divida_liquida, ticker];
-    await conn.query(sql,values);
-    const ey = calculaEarningYeld(empresa.ebit, empresa.valor_mercado, empresa.divida_liquida);
-    const sql2 = 'UPDATE ranking SET earning_yeld = ? WHERE ticker=?;';
-    const values2 = [ey, ticker];
-    await conn.query(sql2,values2);
+async function updateEmpresa(empresa){
+    for(let i = 0; i<empresa.length; i++){
+        const conn = await connect();
+        const sql = 'UPDATE empresa SET nome_empresa = ?, ebit=?,valor_mercado=?,divida_liquida=? WHERE ticker=?';
+        const values =  [empresa[i][1], empresa[i][2], empresa[i][3], empresa[i][4], empresa[i][0]];
+        await conn.query(sql,values);
+        const ey = calculaEarningYeld(empresa[i][2], empresa[i][3], empresa[i][4]);
+        const sql2 = 'UPDATE ranking SET earning_yeld = ? WHERE ticker=?;';
+        const values2 = [ey, empresa[i][0]];
+        await conn.query(sql2,values2);
+    };
+
 }
 
 async function deleteEmpresa(ticker){
@@ -56,13 +87,21 @@ async function deleteEmpresa(ticker){
 async function listarEmpresas(){
     const conn = await connect();
     const [rows] = await conn.query('SELECT * FROM empresa;');
+    console.log("Numero de empresas: " + rows.length);
     return console.log(rows);
 }
 async function rankearEmpresas(){
     const conn = await connect();
     const[rows] = await conn.query('SELECT * FROM ranking ORDER BY earning_yeld DESC');
-    console.log("Esas são as empresas mais baratas da bolsa")
+    console.log("Numero de empresas: " + rows.length);
+    console.log("Ranking das empresas da bolsa");
     return console.log(rows);
 }
+async function topEmpresas(){
+    const conn = await connect();
+    const[rows] = await conn.query('SELECT * FROM ranking ORDER BY earning_yeld DESC LIMIT 10;');
+    console.log(`Essas são as ${rows.length} empresas mais baratas da bolsa`);
+    return rows;
+}
 
-module.exports = {selectEmpresa, insertEmpresa,updateEmpresa, deleteEmpresa, listarEmpresas, rankearEmpresas}
+module.exports = {selectEmpresa, insertEmpresa,updateEmpresa, deleteEmpresa, listarEmpresas, rankearEmpresas, topEmpresas}
--- index.js
+++ index.js
@@ -1,5 +1,156 @@
 const readlineSync = require('readline-sync');
 const db = require("./db");
+const empresas = [["CGRA4", "grazziotin"],
+["ENAT3", "enauta"],
+["JHSF3", "jhsf participacoes"],
+["ODPV3", "odontoprev"],
+ ["SAPR4", "sanepar"],
+ ["TAEE4", "taesa"],
+ ["TRPL4", "transmissão paulista"],
+["WIZS3", "Wiz"]
+,["LAME3", "lojas americanas"]
+,["MGLU3", "Magazine luiza"],
+["WEGE3", "weg"],
+["SLCE3", "Slc agricola"],
+["SBSP3", "CIA Saneamento de São Paulo"],
+["ASAI3", "assai atacadista"], 
+["CPLE3", "Copel"],
+["PCAR3", "Grupo pão de açucar"],
+["HYPE3", "Hypera"],
+["B3SA3", "B3 Sa"],
+["VALE3", "Vale"], 
+["ELET3", "Eletrobras"], 
+["PETR4", "petrobras"],
+["KLBN4", "Klabin"], 
+["ENBR3", "EDP Brasil"], 
+["QUAL3", "Qualicorp"], 
+["NTCO3", "Natura"],
+["STBP3", "Santos Brasil"], 
+["ARZZ3", "Arezzo"], 
+["VIVT3", "Vivo"], 
+["ABEV3", "Ambev"],
+["EPAR3", "Embpar"], 
+["CTNM4", "Tecidos Norte de Minas"], 
+["USIM3", "Usiminas"], 
+["TASA3", "Taurus Armas"], 
+["PSSA3", "Porto seguro"], 
+["PTBL3", "Portobello"], 
+["MRVE3", "MRV engenharia"],
+["LAVV3", "Lavvi"], 
+["TGMA3", "Tegma"], 
+["HBSA3", "Hidrovias do Brasil"], 
+["SEQL3", "Sequoia"],
+["GFSA3", "Gafisa"],  
+["SQIA3", "Sinqia"],  
+["DASA3", "Dasa"],  
+["RCSL4", "recrusul"],  
+["LWSA3", "Locaweb"],  
+["DMVF3", "Varejo Farma"], 
+["CMIN3", "csn mineração"],
+["ALUP3", "Alupar"], 
+["MRFG3", "Marfrig"], 
+["MNPR3", "Minupar"], 
+["DMMO3", "Dommo Energia"],
+["UNIP3", "Unipar"], 
+["LIGT3", "Light SA"], 
+["GRND3", "Grendene"], 
+["SEER3", "Ser Educacional"],
+["ANIM3", "Anima Holding"], 
+["LREN3", "Lojas Renner"], 
+["SHUL4", "Schulz"], 
+["MILS3", "Mills"],
+["RENT3", "Localiza"], 
+["LCAM3", "Locamerica"], 
+["MOVI3", "Movida"], 
+["LJQQ3", "Lojas quero quero"],
+["PETZ3", "Petz"], 
+["ESPA3", "MPM"], 
+["CAMB3", "Cambucci"], 
+["VULC3", "Vulcabras"], 
+["TECN3", "technos"],
+["VIVA3", "Vivara"], 
+["CEDO4", "Cedo Textil"], 
+//["PTNT4", "Petenatti"],
+["SGPS3", "Springs"], 
+["HGTX3", "Hering"], 
+["TFCO4", "Track & Field"], 
+["UCAS3", "Unicasa"], 
+["SOJA3", "Boa Safra"], 
+//["AGRO3", "brasilagro"], 
+["TESA3", "Terra santa"], 
+//["JALL3", "Jalles Machado"], 
+["SMTO3", "São martinho"], 
+["CAML3", "Camil"], 
+["MDIA3", "M.Dias Branco"], 
+["BRFS3", "brf"], 
+["JBSS3", "JBS"], 
+["BEEF3", "Minerva"],
+["CRFB3", "Atacadão"], 
+["GMAT3", "Grupo Matheus"], 
+["BOBR4", "Bombril"],
+["ALSO3", "Aliansce"], 
+["BRPR3", "Br Properties"], 
+["CYRE3", "Cyrela"], 
+["HBRE3", "Realty empreendimentos"],
+["IGTA3", "iguatemi"], 
+["JPSA3", "Jereissati"], 
+["LOGG3", "log comercial"], 
+["MULT3", "Multiplan"],
+["SCAR3", "São Carlos"], 
+["SIMH3", "Simpar"], 
+["EUCA4", "EUCATEX"],
+["RANI4", "IRANI"], 
+["SUZB3", "Suzano"], 
+["BRAP3", "Bradespar"],
+["CSNA3", "siderurgica nacional"], 
+["BRKM3", "BRASKEM"], 
+["MGEL4", "Mangels"], 
+["GOAU3", "Metarlugia gerdau"],
+["GGBR3", "Gerdau"], 
+["ATOM3", "Atom empreendimentos"], 
+["OPCT3", "Oceanpact"], 
+["CSAN3", "Cosan"], 
+["PRIO3", "Petro Rio"], 
+["BRDT3", "Petrobras Distribuidora"], 
+["RECV3", "Petroreconcavo"], 
+["UGPA3", "Ultrapar"], 
+["BLAU3", "Blau Farmaceutica"], 
+["PNVL3", "Dimed"], 
+["PGMN3", "Pague menos"], 
+["OFSA3", "Ouro fino"],
+["PFRM3", "Profarma"],
+["RADL3", "Raia Drogasil"], 
+["AALR3", "Alar"], 
+["FLRY3", "Fleury"], 
+["HAPV3", "Hapvida"], 
+["MATD3", "Hospital mat dei"], 
+["PARD3", "Hermes Pardini"], 
+["GNDI3", "Notre Dame"], 
+["RDOR3", "Rede dor"], 
+["BMOB3", "Bemobi"], 
+["CASH3", "Méliuz"], 
+["MOSI3", "Mosaico tecnologia"],
+["NGRD3", "Neogrid"], 
+["TOTS3", "Totvs"], 
+["POSI3", "Positivo"], 
+["INTB3", "Intelbras"],
+["AMBP3", "Ambipar"], 
+["CSMG3", "Copasa"], 
+["ORVR3", "Orizon"], 
+["TIET3", "AES Tietê"],
+["CMIG4", "Cemig"], 
+["CLSC4", "Celesc"], 
+["CEEB3", "Eletricidade Bahia"], 
+["CPFE3", "cpfl"],
+["KEPL3", "kepler weber"], 
+["EMAE4", "Empresa Metrop"], 
+["ENGI4", "Energisa"], 
+["ENEV3", "Eneva"],
+["EGIE3", "Engie"], 
+["EQTL3", "Equatorial"], 
+["POWE3", "Power"], 
+["NEOE3", "Neoenergia"],
+["OMGE3", "Omega"]];
 function validaFaixaInteiro(pergunta, inicio, fim){
     while(true){
         let valor = readlineSync.question(pergunta);
@@ -13,8 +164,8 @@ function validaFaixaInteiro(pergunta, inicio, fim){
 };
 
 function menu(){
-    console.log("\n ----Menu Principal----\nO que você deseja fazer?\n1 - Inserir Empresa\n2 - Atualizar Empresa\n3 - Excluir Empresa\n4 - Selecionar Empresa\n5 - Listar Empresas\n6 - Rankear ações mais baratas da bolsa\n0 - Sair\n");
-    return validaFaixaInteiro("Digite a opcao que voce deseja: \n", 0, 6);
+    console.log("\n ----Menu Principal----\nO que você deseja fazer?\n1 - Inserir Empresa\n2 - Atualizar Empresa\n3 - Excluir Empresa\n4 - Selecionar Empresa\n5 - Listar Empresas\n6 - Rankear ações mais baratas da bolsa\n7 - Top 10 ações mais baratas\n0 - Sair\n");
+    return validaFaixaInteiro("Digite a opcao que voce deseja: \n", 0, 7);
 };
 
 (async () => {
@@ -23,21 +174,11 @@ function menu(){
         if (escolha == 0){
             break
         }else if(escolha == 1){
-            
-            let ticker = readlineSync.question("\nDigite o ticker da empresa: \n");
-            let nome_empresa = readlineSync.question("\nDigite o nome da empresa: \n");
-            let ebit = Number(readlineSync.question("\nDigite o Ebit da empresa: \n"));
-            let valor_mercado = Number(readlineSync.question("\nDigite o valor de mercado da empresa: \n"));
-            let divida_liquida = Number(readlineSync.question("\nDigite o valor da divida líquida da empresa: \n"));
-            await db.insertEmpresa({ticker: ticker, nome_empresa: nome_empresa, ebit: ebit, valor_mercado: valor_mercado, divida_liquida: divida_liquida});
+
+            await db.insertEmpresa(empresas);
         }else if(escolha == 2){
 
-            let ticker = readlineSync.question("\nDigite o ticker da empresa: \n");
-            let nome_empresa = readlineSync.question("\nDigite o nome da empresa: \n");
-            let ebit = Number(readlineSync.question("\nDigite o Ebit da empresa: \n"));
-            let valor_mercado = Number(readlineSync.question("\nDigite o valor de mercado da empresa: \n"));
-            let divida_liquida = Number(readlineSync.question("\nDigite o valor da divida líquida da empresa: \n"));
-            await db.updateEmpresa(ticker,{nome_empresa: nome_empresa, ebit: ebit, valor_mercado: valor_mercado, divida_liquida: divida_liquida});
+            await db.updateEmpresa(empresas);
         }else if(escolha == 3){
 
             await db.deleteEmpresa(readlineSync.question("\nDigite o ticker da empresa que deseja apagar do sitema: \n"));
@@ -47,7 +188,12 @@ function menu(){
             await db.listarEmpresas();
         }else if(escolha == 6){
             await db.rankearEmpresas();
-        };
+        }else if(escolha == 7){
+            const resposta = await db.topEmpresas();
+            for(let i = 0; i < resposta.length; i++){
+                console.log(`${i+1}`+"°--> " + "Ticker: " + `${resposta[i].ticker} ` + "Earning yeld: " + `${resposta[i].earning_yeld}`);
+            }
+        }
     };
     console.log("\nObrigado por utilizar o Ranking Earning yeld\n");
 })();
